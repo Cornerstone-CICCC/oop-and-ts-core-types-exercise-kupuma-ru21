979a53a78efe5139c06c184b9bd40233448334a1
kupuma-ru21 <tech.kupumaru@gmail.com>
2024-10-03 13:14:48 -0700

    feat: done

--- dist/ex04.js
+++ dist/ex04.js
@@ -1,10 +1,10 @@
+"use strict";
 // Exercise: Create a function `formatInput` that accepts either a string or number as input.
 // If it's a number, return it multiplied by 100. If it's a string, return it in uppercase.
-
-function formatInput(input: number | string) {
-  if (typeof input === "number") return input * 100;
-  return input.toUpperCase();
+function formatInput(input) {
+    if (typeof input === "number")
+        return input * 100;
+    return input.toUpperCase();
 }
-
 console.log(formatInput(5)); // Expected output: 500
 console.log(formatInput("hello")); // Expected output: "HELLO"
--- dist/ex06.js
+++ dist/ex06.js
@@ -1,9 +1,8 @@
+"use strict";
 // Exercise: Define a type alias `Rectangle` with two properties `width` and `height` (both numbers).
 // Then, create a function `calculateArea` that takes a `Rectangle` and returns its area.
-
-function calculateArea({ width, height }: { width: number; height: number }) {
-  return width * height;
+function calculateArea({ width, height }) {
+    return width * height;
 }
-
 console.log(calculateArea({ width: 10, height: 5 }));
 // Expected output: 50
